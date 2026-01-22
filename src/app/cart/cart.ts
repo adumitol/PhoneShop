9db06cc74ca90dc285918ae58e4b5bd35f5637b7
adumitol <aroubel230@g.educaand.es>
2026-01-22 14:32:43 +0100

    Shipping prices

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import {RouterModule} from "@angular/router";
+import { CartService } from '../cart.service';
+import {CurrencyPipe} from "@angular/common";
 
 @Component({
   selector: 'app-cart',
     standalone: true,
-  imports: [RouterModule],
+    imports: [RouterModule, CurrencyPipe],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
 export class Cart {
+    items = this.cartService.getItems();
 
+    constructor(
+        private cartService: CartService
+    ) { }
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,7 @@ import {ProductListComponent} from "./product-list/product-list.component";
 import {Routes} from "@angular/router";
 import {ProductDetailsComponent} from "./product-details/product-details";
 import {Cart} from "./cart/cart";
+import {Shipping} from "./shipping/shipping";
 
 
 const routeConfig: Routes = [
@@ -19,6 +20,10 @@ const routeConfig: Routes = [
         path: 'cart',
         component: Cart,
         title: 'Checkout'
+    },
+    { path: 'shipping',
+        component: Shipping,
+        title: 'Shipping Prices'
     }];
 
 export default routeConfig;
