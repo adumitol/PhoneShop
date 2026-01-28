f1b37b4daee66fef2b54527946d570972189c200
adumitol <aroubel230@g.educaand.es>
2026-01-28 08:38:20 +0100

    todo menos proveedores

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
-import {RouterModule} from "@angular/router";
+import { RouterModule } from "@angular/router";
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { CartService } from '../cart.service';
-import {CurrencyPipe} from "@angular/common";
+import { CurrencyPipe } from "@angular/common";
 
 @Component({
-  selector: 'app-cart',
+    selector: 'app-cart',
     standalone: true,
-    imports: [RouterModule, CurrencyPipe],
-  templateUrl: './cart.html',
-  styleUrl: './cart.css',
+    imports: [
+        RouterModule,
+        CurrencyPipe,
+        ReactiveFormsModule
+    ],
+    templateUrl: './cart.html',
+    styleUrl: './cart.css',
 })
 export class Cart {
+    // Inicializamos las propiedades directamente
     items = this.cartService.getItems();
+
+    checkoutForm = this.formBuilder.group({
+        name: '',
+        address: ''
+    });
+
     constructor(
-        private cartService: CartService
+        private cartService: CartService,
+        private formBuilder: FormBuilder,
     ) { }
 
-
-
-
+    onSubmit(): void {
+        // Procesar datos de pago
+        this.items = this.cartService.clearCart();
+        console.warn('Your order has been submitted', this.checkoutForm.value);
+        this.checkoutForm.reset();
+    }
 }
